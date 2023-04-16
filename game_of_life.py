a789173a67de2732618acd3dc5a3cda3a0b1b4ee
Aypisam <the.aypisam.fpv@gmail.com>
2023-04-16 22:49:45 +0200

    updated graphics

--- game_of_life.py
+++ game_of_life.py
@@ -1,30 +1,40 @@
 import random
 import time
-import numpy as np
+
 import cv2 as cv
+import numpy as np
+
+"""Game of life's settings"""
+grid_size = 16, 32
+rand_grid_coef = 3
+screen_size_coefficient = 20
+"""-----------------------"""
 
 
 def create_grid(rows, columns):
-    coef = 3
     grid = []
     for row in range(rows):
         grid.append([])
         for column in range(columns):
-            grid[row].append(1 if random.randint(0, coef) == coef else 0)
+            grid[row].append(1 if random.randint(
+                0, rand_grid_coef) == rand_grid_coef else 0)
     return grid
 
 
-def get_neighbours(grid, row, column):
+def get_neighbours(grid, rows, columns):
     neighbours = 0
-    for r in range(row - 1, row + 2):
-        for c in range(column - 1, column + 2):
-            if r == row and c == column:
+    for row in range(rows - 1, rows + 2):
+        for column in range(columns - 1, columns + 2):
+            if row == rows and column == columns:
                 continue
-            if r >= len(grid):
-                r = 0
-            if c >= len(grid[r]):
-                c = 0
-            if grid[r][c] == 1:
+            
+            if row >= len(grid):
+                row = 0
+
+            if column >= len(grid[row]):
+                column = 0
+
+            if grid[row][column] == 1:
                 neighbours += 1
     return neighbours
 
@@ -41,27 +51,36 @@ def update_cells(grid):
                 new_grid[row].append(1)
             else:
                 new_grid[row].append(0)
+
     return new_grid
 
 
 def randomness(grid):
     for row in range(len(grid)):
         for column in range(len(grid[row])):
-            if random.randint(0, 50) == 50:
+            if random.randint(0, rand_grid_coef*5) == rand_grid_coef*5:
                 grid[row][column] = 1
+
     return grid
 
 
 def grid_to_screen(grid):
     '''Convert a grid into a cv2 screen'''
-    screen_size_coefficient = 25
     screen = np.zeros((len(grid)*screen_size_coefficient,
                       len(grid[0])*screen_size_coefficient, 1), dtype=np.uint8)
+
     for row in range(len(grid)):
         for column in range(len(grid[row])):
+            screen[row*screen_size_coefficient:(row+1)*screen_size_coefficient,
+                   column*screen_size_coefficient:(column+1)*screen_size_coefficient] = 25
+
             if grid[row][column] == 1:
-                screen[row*screen_size_coefficient:(row+1)*screen_size_coefficient,
-                       column*screen_size_coefficient:(column+1)*screen_size_coefficient] = 255
+                screen[(row*screen_size_coefficient)+1:(row+1)*screen_size_coefficient,
+                       (column*screen_size_coefficient)+1:(column+1)*screen_size_coefficient] = 255
+            else:
+                screen[(row*screen_size_coefficient)+1:(row+1)*screen_size_coefficient,
+                       (column*screen_size_coefficient)+1:(column+1)*screen_size_coefficient] = 0
+
     return screen
 
 
@@ -88,5 +107,5 @@ def main(grid):
 
 
 if __name__ == '__main__':
-    grid = create_grid(50, 50)
+    grid = create_grid(grid_size[0], grid_size[1])
     main(grid)
